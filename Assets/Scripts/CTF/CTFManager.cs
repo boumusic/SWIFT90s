4978ff3336682966b8ff2a91af564e2e8f6c1ebb
Nicolas <nicoceriani@gmail.com>
2020-04-28 19:40:58 +0200

    UI SPeedrun

---
+++ Assets/Scripts/CTF/CTFManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTFManager : MonoBehaviour
+{
+    private static CTFManager instance;
+    public static CTFManager Instance
+    {
+        get { if (!instance) instance = FindObjectOfType<CTFManager>(); return instance; }
+    }
+
+    [Header("Gameplay Rules")]
+    public int minutes = 5;
+    public int seconds = 0;
+    public int goalPoints = 3;
+
+    private Timer timer;
+    public Timer Timer => timer;
+
+    private void Start()
+    {
+        timer = new Timer(minutes, seconds, TimerOver);
+        timer.Start();
+    }
+
+    private void Update()
+    {
+        timer.Update();
+    }
+
+    private void TimerOver()
+    {
+        Debug.Log("Time is up !");
+    }
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private void Update()
+    {
+        text.text = CTFManager.Instance.Timer.GetTimeLeftString();
+    }
+}
